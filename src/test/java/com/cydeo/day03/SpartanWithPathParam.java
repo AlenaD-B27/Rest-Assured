dcbd28b7e8ab8f9d45c31c0d8076f97fd5b08245
AlenaD-B27 <lenador.18@gmail.com>
2022-12-05 11:30:25 -0600

    cleaned imports

--- src/test/java/com/cydeo/day03/SpartanWithPathParam.java
+++ src/test/java/com/cydeo/day03/SpartanWithPathParam.java
@@ -1,16 +1,13 @@
 package com.cydeo.day03;
 
 import com.cydeo.utilities.SpartanTestBase;
-import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.*;
