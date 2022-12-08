feb92d8eab21958f1b0722e9f771e3d792e3ca49
AlenaD-B27 <lenador.18@gmail.com>
2022-12-07 23:04:18 -0600

    HW-3 is done

--- src/test/java/com/cydeo/day04/P03_ZipcodeTest.java
+++ src/test/java/com/cydeo/day04/P03_ZipcodeTest.java
@@ -1,15 +1,20 @@
 package com.cydeo.day04;
 
-public class P03_ZipcodeTest {
+import com.cydeo.utilities.ZipcodeBase;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class P03_ZipcodeTest extends ZipcodeBase {
     //https://www.zippopotam.us/#
 
-    /**
-     * Create TestBase for zipcode
-     *
-     *
-     */
 
-    /**
+    /*
      Given accept type is json
      and country path param value is "us"
      and postal code path param value is 22102
@@ -20,7 +25,26 @@ public class P03_ZipcodeTest {
      And "place name" is "Mc Lean"
      And  "state" is "Virginia"
      */
-    //TODO
+
+    @DisplayName("GET /{country}/{postal-code}")
+    @Test
+    public void test1(){
+
+        Response response = given().accept(ContentType.JSON)
+                .and().pathParam("country", "US")
+                .and().pathParam("postal-code", 22102)
+                .when().get("/{country}/{postal-code}");
+
+        assertEquals(200,response.statusCode());
+
+        JsonPath jsonPath = response.jsonPath();
+
+        assertEquals(22102, jsonPath.getInt("'post code'"));
+        assertEquals("United States", jsonPath.getString("country"));
+        assertEquals("Mc Lean", jsonPath.getString("places[0].'place name'"));
+        assertEquals("Virginia",jsonPath.getString("places[0].state"));
+
+    }
 
 
 
