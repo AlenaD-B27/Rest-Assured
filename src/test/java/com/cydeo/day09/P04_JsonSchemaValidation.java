dce396da7bbf2586dec4514e42ad78f06cb716d6
AlenaD-B27 <lenador.18@gmail.com>
2022-12-19 15:47:00 -0600

    Json Schema Validation intro

---
+++ src/test/java/com/cydeo/day09/P04_JsonSchemaValidation.java
@@ -0,0 +1,47 @@
+package com.cydeo.day09;
+
+import com.cydeo.utilities.SpartanTestBase;
+import io.restassured.http.ContentType;
+import io.restassured.module.jsv.JsonSchemaValidator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
+
+public class P04_JsonSchemaValidation extends SpartanTestBase {
+
+    @DisplayName("GET /api/spartans/{id} to validate with JsonSchemaValidator")
+    @Test
+    public void test1() {
+
+        given().accept(ContentType.JSON)
+                .pathParam("id",45)
+                .when().get("/api/spartans/{id}").prettyPeek()
+
+                .then().statusCode(200)
+                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("SingleSpartanSchema.json")); // source root
+
+
+
+    }
+
+    @DisplayName("GET /api/spartans/{id} to validate with JsonSchemaValidator matchesJsonSchema")
+    @Test
+    public void test2() {
+
+        given().accept(ContentType.JSON)
+                .pathParam("id",45)
+                .when().get("/api/spartans/{id}").prettyPeek()
+
+                .then().statusCode(200)
+                .body(JsonSchemaValidator.matchesJsonSchema(new File("src/test/resources/SingleSpartanSchema.json"))); // content root
+
+
+
+    }
+}
