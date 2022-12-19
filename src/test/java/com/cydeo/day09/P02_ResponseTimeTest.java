6634979e718848c371888b1c7aafd596c3ddaa62
AlenaD-B27 <lenador.18@gmail.com>
2022-12-19 13:58:20 -0600

    Response time test

---
+++ src/test/java/com/cydeo/day09/P02_ResponseTimeTest.java
@@ -0,0 +1,36 @@
+package com.cydeo.day09;
+
+import com.cydeo.utilities.SpartanAuthTestBase;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
+
+public class P02_ResponseTimeTest extends SpartanAuthTestBase {
+
+    @DisplayName("GET /api/spartans to get response time")
+    @Test
+    public void test1() {
+
+        Response response = given().accept(ContentType.JSON)
+                .auth().basic("admin", "admin").
+                when().get("/api/spartans")
+                .then().statusCode(200)
+                .contentType(ContentType.JSON)
+                .time(both(greaterThan(100L)).and(lessThan(600L)))
+                .extract().response();
+
+        System.out.println(response.getTimeIn(TimeUnit.MILLISECONDS));
+
+
+    }
+
+
+}
