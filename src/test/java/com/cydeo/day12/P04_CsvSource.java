4c9053de71970717ebdaa2cd54db3c8d376a0f63
AlenaD-B27 <lenador.18@gmail.com>
2022-12-20 11:59:18 -0600

    CsvSource continue

--- src/test/java/com/cydeo/day12/P04_CsvSource.java
+++ src/test/java/com/cydeo/day12/P04_CsvSource.java
@@ -1,8 +1,11 @@
 package com.cydeo.day12;
 
+import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+
+import static io.restassured.RestAssured.given;
 
 public class P04_CsvSource {
     @ParameterizedTest
@@ -14,5 +17,38 @@ public class P04_CsvSource {
         System.out.println(num1 + " + " + num2 + " = " + sum);
         Assertions.assertEquals(sum, (num1+num2));
     }
+
+    // Write a parameterized test for this request
+    // GET https://api.zippopotam.us/us/{state}/{city}
+    /*
+        "NY, New York",
+        "CO, Denver",
+        "VA, Fairfax",
+        "MA, Boston",
+        "NY, New York",
+        "MD, Annapolis"
+     */
+    //verify place name contains your city name
+    //print number of places for each request
+
+    @ParameterizedTest
+    @CsvSource({"NY, New York",
+            "CO, Denver",
+            "VA, Fairfax",
+            "MA, Boston",
+            "MD, Annapolis"})
+    public void test1(String state, String city){
+        JsonPath jsonPath = given().baseUri("https://api.zippopotam.us")
+                .pathParam("state", state)
+                .pathParam("city", city)
+                .when().get("us/{state}/{city}")//.prettyPeek()
+                .then().statusCode(200)
+                .extract().jsonPath();
+
+        Assertions.assertTrue(jsonPath.getList("places.'place name'").stream().allMatch(p -> p.toString().contains(city)));
+        System.out.println(city + " has " + jsonPath.getList("places").size());
+    }
+
+
 
 }
