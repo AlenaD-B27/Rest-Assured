25381a58bbf2750bf05f944963eb58eb371660f2
AlenaD-B27 <lenador.18@gmail.com>
2022-12-21 22:20:08 -0600

    Homework 2 is done

--- src/test/java/com/cydeo/day12/P09_SpartanPOSTDDT.java
+++ src/test/java/com/cydeo/day12/P09_SpartanPOSTDDT.java
@@ -1,6 +1,16 @@
 package com.cydeo.day12;
 
-public class P09_SpartanPOSTDDT {
+import com.cydeo.pojo.Spartan;
+import com.cydeo.utilities.SpartanTestBase;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import static io.restassured.RestAssured.*;
+
+public class P09_SpartanPOSTDDT extends SpartanTestBase {
 
     /**
      *
@@ -10,7 +20,7 @@ public class P09_SpartanPOSTDDT {
      *  And accept type is json
      *  When I POST Spartan
      *  And status code needs to 201
-     *  Verify spartan informations macthing with dynamic taht we provide
+     *  Verify spartan information is matching dynamic that we provide
      *
      *  Generate DUMMY DATA
      *  https://www.mockaroo.com/
@@ -21,7 +31,29 @@ public class P09_SpartanPOSTDDT {
      *
      */
 
-    //TODO
+    @ParameterizedTest
+    @CsvFileSource(resources = "/newbornSpartans.csv", numLinesToSkip = 1)
+    public void spartansBirthday(String name, String gender, long phone){
+
+        Spartan newSpartan = new Spartan();
+        newSpartan.setName(name);
+        newSpartan.setGender(gender);
+        newSpartan.setPhone(phone);
+
+        JsonPath jsonPath = given().accept(ContentType.JSON).contentType(ContentType.JSON)
+                .body(newSpartan)
+                .when().post("/api/spartans")//.prettyPeek()
+                .then().statusCode(201)
+                .extract().jsonPath();
+
+        Spartan actualSpartanFromResponse = jsonPath.getObject("data", Spartan.class);
+
+
+        Assertions.assertEquals(newSpartan.getName(), actualSpartanFromResponse.getName());
+        Assertions.assertEquals(newSpartan.getGender(),actualSpartanFromResponse.getGender());
+        Assertions.assertEquals(newSpartan.getPhone(),actualSpartanFromResponse.getPhone());
+
+    }
 
 
 }
