5f9fc3e840a60c635bca0a2f65e402088e9edc66
AlenaD-B27 <lenador.18@gmail.com>
2022-12-22 11:13:35 -0600

    Specifications dynamicly practice

--- src/test/java/com/cydeo/day13/P02_SpartanSpecTest.java
+++ src/test/java/com/cydeo/day13/P02_SpartanSpecTest.java
@@ -5,8 +5,9 @@ import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
-import static com.cydeo.utilities.NewsTestBase.*;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 public class P02_SpartanSpecTest extends SpartanNewTestBase {
@@ -63,6 +64,43 @@ public class P02_SpartanSpecTest extends SpartanNewTestBase {
                 .then().spec(dynamicResSpec(200));
 
     }
+
+    /**
+     *  Create GET_RBAC.csv
+     *   username,password,id,statuscode
+     *    admin,admin,3,200
+     *    editor,editor,3,200
+     *    user,user,3,200
+     *
+     *  Create a parameterized test to check RBAC for GET method
+     *
+     *
+     */
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/GET_RBAC.csv", numLinesToSkip = 1)
+    public void checkAll_RBAC_forGET_Method(String username, String password,int id,int statusCode){
+
+        given().spec(dynamicReqSpec(username,password))
+                .pathParam("id", id)
+                .when().get("/spartans/{id}")
+                .then().spec(dynamicResSpec(statusCode));
+
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/DELETE_RBAC.csv", numLinesToSkip = 1)
+    public void checkAll_RBAC_ForDeleteMethod(String username, String password,int id,int statusCode){
+
+        given().spec(dynamicReqSpec(username,password))
+                .pathParam("id", id)
+                .when().delete("/spartans/{id}")
+                .then().spec(dynamicResSpec(statusCode));
+
+    }
+
+
+
 
 
 
