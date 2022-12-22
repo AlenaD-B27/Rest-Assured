56d0a908e1204f193abbc2dd4e57c53ab91c9e9c
AlenaD-B27 <lenador.18@gmail.com>
2022-12-22 11:24:08 -0600

    Bookit utils and properties added

--- src/test/java/com/cydeo/utilities/BookitUtils.java
+++ src/test/java/com/cydeo/utilities/BookitUtils.java
@@ -2,6 +2,9 @@ package com.cydeo.utilities;
 
 import io.restassured.http.ContentType;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
@@ -21,6 +24,41 @@ public class BookitUtils {
 
 
         return "Bearer " + accessToken;
+    }
+
+    public static String getTokenByRole(String role) {
+        String email = "";
+        String password = "";
+
+        switch (role) {
+            case "teacher":
+                email = ConfigurationReader.getProperty("teacher_email");
+                password = ConfigurationReader.getProperty("teacher_password");
+                break;
+
+            case "student-member":
+                email = ConfigurationReader.getProperty("team_member_email");
+                password = ConfigurationReader.getProperty("team_member_password");
+                break;
+            case "student-leader":
+                email = ConfigurationReader.getProperty("team_leader_email");
+                password = ConfigurationReader.getProperty("team_leader_password");
+                break;
+            default:
+
+                throw new RuntimeException("Invalid Role Entry :\n>> " + role +" <<");
+        }
+
+        Map<String, String> credentials = new HashMap<>();
+        credentials.put("email", email);
+        credentials.put("password", password);
+
+        String accessToken = given()
+                .queryParams(credentials)
+                .when().get( "/sign").path("accessToken");
+
+        return  "Bearer " + accessToken;
+
     }
 
 
